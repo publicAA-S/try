4298296ac13490e78abac492204c9ee1f8adac71
publicAA-S <anant.singh.public.use@gmail.com>
2025-02-07 00:19:15 +0530

    Update b.js

--- b.js
+++ b.js
@@ -1,7 +1,7 @@
 const rssUrl = "https://news.google.com/rss/search?q=programming";
 
 async function a () {
-    console.log(await (await fetch(rssUrl, { mode: 'no-cors' })).text());
+    console.log(await (await fetch(rssUrl)).text());
 }
 
 a()
